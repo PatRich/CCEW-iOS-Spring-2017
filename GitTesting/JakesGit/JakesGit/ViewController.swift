b9dd5fd61e9b66f44c230f51ef073e353f9f5f84
Jake Manning <jakemanning@ou.edu>
2017-02-09 02:48:28 -0600

    Adds my project

---
+++ GitTesting/JakesGit/JakesGit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  JakesGit
+//
+//  Created by Jake Manning on 2/9/17.
+//  Copyright © 2017 ccew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Git")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
